0c6d167af7f414c11f5f41a26c9726c6465216c8
pedroveracsv <pedrovera040504@gmail.com>
2025-01-05 00:14:04 -0300

    Descripción de los cambios

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portafolio/', // Asegúrate de que coincida con el nombre del repositorio
 })
